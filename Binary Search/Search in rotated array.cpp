e47c35a29ff6f4ad3da069ef4a60253ba0fe5dae
Dheeraj Barman <123076646+Dheeraj431@users.noreply.github.com>
2023-05-12 13:12:02 +0530

    Create Search in rotated array.cpp

---
+++ Binary Search/Search in rotated array.cpp
@@ -0,0 +1,32 @@
+class Solution{
+    public:
+    int search(int arr[], int l, int h, int key){
+        int low = l,high = h;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            // left side is sorted;
+            
+            if(arr[mid]==key)
+                return mid;
+            
+            if(arr[low]<=arr[mid]){
+                if(key>=arr[low] && key<=arr[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            //right side is sorted
+            else{
+                if(arr[mid]<=key && arr[high]>=key){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
