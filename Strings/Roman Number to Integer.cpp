b17a5b485db28d0aa5c4b3189a77fd39c12809c3
Dheeraj Barman <123076646+Dheeraj431@users.noreply.github.com>
2023-05-18 13:46:20 +0530

    Create Roman Number to Integer.cpp
    rules of roman numbers

---
+++ Strings/Roman Number to Integer.cpp
@@ -0,0 +1,29 @@
+//User function template for C++
+
+class Solution {
+  public:
+    int romanToDecimal(string &str) {
+        map<char,int>mp;
+        mp['I']=1;
+        mp['V']=5;
+        mp['X']=10;
+        mp['L']=50;
+        mp['C']=100;
+        mp['D']=500;
+        mp['M']=1000;
+        int n=str.size();
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(mp[str[i]]<mp[str[i+1]])
+            {
+                sum-=mp[str[i]];
+            }
+            else{
+                sum+=mp[str[i]];
+            }
+        }
+        
+        return sum;
+    }
+};
