6381c681f1082bcbd002b0b7905c354f92d8cb7c
Dheeraj Barman <123076646+Dheeraj431@users.noreply.github.com>
2023-06-05 21:14:26 +0530

    Update dfs.cpp

--- graph/dfs.cpp
+++ graph/dfs.cpp
@@ -1,6 +1,7 @@
 class Solution {
   public:
     // Function to return a list containing the DFS traversal of the graph.
+   //WITH LIST
     void dfs(int node, vector<int>&visited, vector<int>&seq,vector<int>adj[]){
         if(visited[node])
             return;
@@ -10,6 +11,15 @@ class Solution {
             dfs(u,visited,seq,adj);
         }
     }
+  //WITH GRID
+    void dfs(int node, vector<bool>& visited, vector<vector<int>>& adj) {
+    visited[node] = true;
+    for (int i = 0; i < adj.size(); i++) {
+        if (adj[node][i] && !visited[i]) {
+            dfs(i, visited, adj);
+        }
+    }
+}
     vector<int> dfsOfGraph(int V, vector<int> adj[]) {
         // Code here
         vector<int>visited(V);
