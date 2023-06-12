5361391945d0e349db8a8f5cb58b928ff68254cd
Dheeraj Barman <123076646+Dheeraj431@users.noreply.github.com>
2023-06-12 11:53:18 +0530

    Create topo sort using BFS.cpp

---
+++ graph/topo sort using BFS.cpp
@@ -0,0 +1,30 @@
+//Problem Link : https://practice.geeksforgeeks.org/problems/topological-sort/1
+vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    vector<int>indeg(V);
+	    for(int node=0; node<V; node++){
+	        for(auto u:adj[node]){
+	            indeg[u]++;
+	        }
+	    }
+	    queue<int>q;
+	    for(int i=0; i<V; i++){
+	        if(indeg[i]==0){
+	            q.push(i);
+	        }
+	    }
+	    vector<int>topo; 
+	    while(!q.empty()){
+	        int x=q.front();
+	        q.pop();
+	        topo.push_back(x);
+	        for(auto u:adj[x]){
+	            indeg[u]--;
+	            if(indeg[u]==0){
+	                q.push(u);
+	            }
+	        }
+	    }
+	    return topo;
+	}
